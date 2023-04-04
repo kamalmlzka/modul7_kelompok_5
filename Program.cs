bdf131e6116588350f1e214923045bd8a2cb8993
Vanzher77 <rizkyevan2002@gmail.com>
2023-04-04 16:36:09 +0700

    Menambahkan Class Member

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+
+public class program
+{
+    public static Member readJSON(string filename)
+    {
+        string posisifolder = ".";
+        string hasilbaca = File.ReadAllText(posisifolder + "\\" + filename);
+        return JsonSerializer.Deserialize<Member>(hasilbaca);
+    }
+
+    private static void Main(string[] args)
+    {
+        Member Members = readJSON("jurnal7_2_1302213058.json");
+        for(int i = 0; i < Members.members.Count; i++)
+        {
+            Console.WriteLine("Nama Awal: " + Members.members[i].firstName + " Nama akhir: " + Members.members[i].lastName + " Kelamin: " + Members.members[i].gender + " Umur: " + Members.members[i].age + " Nim: " + Members.members[i].nim);
+        }
+    }
+}
+ 
