2a8fee3d5f612c81ee7bf83a55545adc78556f1b
logicrius <naziqnaz13@gmail.com>
2023-04-04 16:36:14 +0700

    menambahkan class Mahasiswa
    menambahkan class Mahasiswa

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+
+public class program
+{
+    private static void Main(string[] args)
+    {
+        Mahasiswa Mhs = ReadJSON("Jurnal7_1_1302210104.json");
+
+        Console.WriteLine("Name : " + Mhs.firstName + " " + Mhs.lastName);
+        
+        Console.WriteLine("Gender : " + Mhs.gender);
+        Console.WriteLine("Umur : " + Mhs.age);
+        Console.WriteLine("Alamat : " + Mhs.address.streetAddress + ", " + Mhs.address.city + ", " + Mhs.address.state);
+        Console.WriteLine("Courses : ");
+        for(int i = 0; i< Mhs.courses.Count; i++)
+        {
+            Console.WriteLine("Mata Kuliah " + (i + 1) + " : " + Mhs.courses[i].code + " " + Mhs.courses[i].name);
+        }
+    }
+
+    public static Mahasiswa ReadJSON(string filename)
+    {
+        string posisifolder = ".";
+        string LetakFile = File.ReadAllText(posisifolder+"\\"+filename);
+        return JsonSerializer.Deserialize<Mahasiswa>(LetakFile);
+    }
+}
